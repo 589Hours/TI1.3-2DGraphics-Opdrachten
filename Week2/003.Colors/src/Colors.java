67989710da0689ca37d4d560b62561f1f7c603f3
589Hours <rik.vanham@ziggo.nl>
2024-02-05 11:47:21 +0100

    Colors opdracht stable

--- Week2/003.Colors/src/Colors.java
+++ Week2/003.Colors/src/Colors.java
@@ -1,5 +1,7 @@
 import java.awt.*;
 import java.awt.geom.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 import javafx.application.Application;
 
@@ -33,6 +35,33 @@ public class Colors extends Application {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+        ArrayList<Color> colors = new ArrayList<>();
+
+        double x = 0;
+        double y = 0;
+        double width = 100;
+        double height = 100;
+        colors.add(Color.white);
+        colors.add(Color.black);
+        colors.add(Color.blue);
+        colors.add(Color.red);
+        colors.add(Color.green);
+        colors.add(Color.yellow);
+        colors.add(Color.orange);
+        colors.add(Color.cyan);
+        colors.add(Color.darkGray);
+        colors.add(Color.gray);
+        colors.add(Color.lightGray);
+        colors.add(Color.magenta);
+        colors.add(Color.pink);
+
+        for (int i = 0; i < 13; i++) {
+            Area square = new Area(new Rectangle2D.Double(x,y,width,height));
+            x+=width+1;
+            graphics.setColor(colors.get(i));
+            graphics.fill(square);
+        }
+
     }
 
 
