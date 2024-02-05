29535494c0697934d5e6b4248d3cefb8758f47cc
589Hours <rik.vanham@ziggo.nl>
2024-02-05 17:51:00 +0100

    Gradient paint finished

--- Week2/004.GradientPaint/src/GradientPaintExercise.java
+++ Week2/004.GradientPaint/src/GradientPaintExercise.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.awt.geom.*;
+import java.util.ArrayList;
 
 import javafx.application.Application;
 
@@ -24,15 +25,30 @@ public class GradientPaintExercise extends Application {
         primaryStage.setScene(new Scene(mainPane));
         primaryStage.setTitle("GradientPaint");
         primaryStage.show();
+
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
+
+        canvas.setOnMouseMoved(event -> {
+            position = new Point2D.Double(event.getX(), event.getY());
+            draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
+        });
     }
 
-
+    public Point2D position = new Point2D.Double(1920/2,1080/2);
     public void draw(FXGraphics2D graphics)
     {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+
+        Color[] colors = new Color[]{Color.BLUE, Color.white,Color.red};
+        float[] fractions = new float[]{0.3f, 0.7f, 0.9f};
+        Area rectangle = new Area(new Rectangle2D.Double(0, 0,canvas.getWidth(),canvas.getHeight()));
+
+        RadialGradientPaint radialGradientPaint =
+                new RadialGradientPaint(position, 100, fractions, colors, MultipleGradientPaint.CycleMethod.NO_CYCLE);
+        graphics.setPaint(radialGradientPaint);
+        graphics.fill(rectangle);
     }
 
 
