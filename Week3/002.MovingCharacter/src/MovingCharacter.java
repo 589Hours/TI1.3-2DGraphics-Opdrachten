8eb900e27f6d8b1c9f300e40b8650186e21259f8
589Hours <rik.vanham@ziggo.nl>
2024-02-19 22:40:12 +0100

    Moving character
    De opdracht werkt. heb nog wel een kleine bug dat hij op het einde van het lopen "kneeled". Maar ik snap alleen niet waarom.

--- Week3/002.MovingCharacter/src/MovingCharacter.java
+++ Week3/002.MovingCharacter/src/MovingCharacter.java
@@ -1,15 +1,14 @@
 import java.awt.*;
 import java.awt.geom.*;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 
 import static javafx.application.Application.launch;
 
-import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -20,15 +19,27 @@ import org.jfree.fx.ResizableCanvas;
 
 public class MovingCharacter extends Application {
     private ResizableCanvas canvas;
+    private BufferedImage[] tiles;
+    private int[] walkAnimation;
+    private int[] jumpAnimation;
+    private int x = 0;
+    private int tileChoice = 0;
+    private int animationChoice = 0;
+    private int timer = 0;
+    private boolean jumpAnimationActive = false;
 
     @Override
     public void start(Stage stage) throws Exception
     {
-
         BorderPane mainPane = new BorderPane();
         canvas = new ResizableCanvas(g -> draw(g), mainPane);
         mainPane.setCenter(canvas);
         FXGraphics2D g2d = new FXGraphics2D(canvas.getGraphicsContext2D());
+        getSubImages();
+        getWalkAnimationIntegers();
+        getJumpAnimationIntegers();
+        canvas.setOnMouseClicked(event -> mouseClick(event));
+        canvas.setOnMouseReleased(event -> mouseRelease(event));
         new AnimationTimer() {
             long last = -1;
 
@@ -49,22 +60,81 @@ public class MovingCharacter extends Application {
         draw(g2d);
     }
 
-
     public void draw(FXGraphics2D graphics)
     {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+
+        Image image = tiles[tileChoice];
+        graphics.drawImage(image, x, (int) (canvas.getHeight()/2), null);
     }
 
 
     public void update(double deltaTime)
     {
+        timer++;
+        if(timer > 10) {
+            if (!jumpAnimationActive) {
+                x+=2;
+                tileChoice = walkAnimation[animationChoice];
+                if (animationChoice == 8) {
+                    animationChoice = 0;
+                } else {
+                    animationChoice++;
+                }
+            } else {
+                tileChoice = jumpAnimation[animationChoice];
+                if (animationChoice == 8) {
+                    animationChoice = 0;
+                    jumpAnimationActive = false;
+                } else {
+                    animationChoice++;
+                }
+            }
+            timer = 0;
+        }
     }
 
     public static void main(String[] args)
     {
         launch(MovingCharacter.class);
     }
+
+    public void getSubImages() {
+        try {
+            BufferedImage image = ImageIO.read(getClass().getResource("/images/sprite.png"));
+            tiles = new BufferedImage[65];
+            //knip de afbeelding op in 65 stukjes van 64x64 pixels.
+            for (int i = 0; i < 65; i++)
+                tiles[i] = image.getSubimage(64 * (i % 8), 64 * (i / 8), 64, 64);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private void getWalkAnimationIntegers() {
+        walkAnimation = new int[9];
+        int j = 33;
+        for (int i = 0; i < 9; i++) {
+            walkAnimation[i] = j;
+            j++;
+        }
+    }
+    private void getJumpAnimationIntegers(){
+        jumpAnimation = new int[9];
+        int j = 40;
+        for (int i = 0; i < 8; i++) {
+            jumpAnimation[i] = j;
+            j++;
+        }
+    }
+    private void mouseClick(MouseEvent event) {
+        jumpAnimationActive = true;
+        animationChoice = 0;
+        tileChoice = 64;
+    }
+    private void mouseRelease(MouseEvent event) {
+    }
+
 
 }
