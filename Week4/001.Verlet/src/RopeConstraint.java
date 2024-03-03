6030f0b27c669e57bb457ad6d32c2c2151bf33ac
589Hours <rik.vanham@ziggo.nl>
2024-03-03 23:17:52 +0100

    Verlet engine
    opdrachten af behalve: ropeConstraint, doek en save to file

--- Week4/001.Verlet/src/RopeConstraint.java
+++ Week4/001.Verlet/src/RopeConstraint.java
@@ -4,25 +4,23 @@ import java.awt.*;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 
-public class DistanceConstraint implements Constraint {
-
+public class RopeConstraint implements Constraint{
     private double distance;
     private Particle a;
     private Particle b;
 
-    public DistanceConstraint(Particle a, Particle b) {
+    public RopeConstraint(Particle a, Particle b) {
         this(a, b, a.getPosition().distance(b.getPosition()));
     }
 
-    public DistanceConstraint(Particle a, Particle b, double distance) {
+    public RopeConstraint(Particle a, Particle b, double distance) {
+        this.distance = distance;
         this.a = a;
         this.b = b;
-        this.distance = distance;
     }
 
     @Override
     public void satisfy() {
-
         double currentDistance = a.getPosition().distance(b.getPosition());
         double adjustmentDistance = (currentDistance - distance) / 2;
 
@@ -32,7 +30,7 @@ public class DistanceConstraint implements Constraint {
         {
             BA = new Point2D.Double(BA.getX() / length, BA.getY() / length);
         } else {
-            BA = new Point2D.Double(1, 0);
+            BA = new Point2D.Double(BA.getX(), BA.getY());
         }
 
         a.setPosition(new Point2D.Double(a.getPosition().getX() + BA.getX() * adjustmentDistance,
@@ -43,10 +41,7 @@ public class DistanceConstraint implements Constraint {
 
     @Override
     public void draw(FXGraphics2D g2d) {
-        //get a color based on the power on the constraint
         g2d.setColor(getColor());
-
-        //draw the constraint
         g2d.draw(new Line2D.Double(a.getPosition(), b.getPosition()));
     }
 
@@ -72,5 +67,16 @@ public class DistanceConstraint implements Constraint {
         } else {
             return Color.red;
         }
+    }
+    public double getDistance() {
+        return distance;
+    }
+
+    public Particle getA() {
+        return a;
+    }
+
+    public Particle getB() {
+        return b;
     }
 }
--- Week4/001.Verlet/src/VerletEngine.java
+++ Week4/001.Verlet/src/VerletEngine.java
@@ -1,17 +1,23 @@
 
 import java.awt.*;
 import java.awt.geom.*;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
+
+import javafx.event.ActionEvent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
 import org.jfree.fx.FXGraphics2D;
 import org.jfree.fx.ResizableCanvas;
 
@@ -21,10 +27,24 @@ public class VerletEngine extends Application {
     private ArrayList<Particle> particles = new ArrayList<>();
     private ArrayList<Constraint> constraints = new ArrayList<>();
     private PositionConstraint mouseConstraint = new PositionConstraint(null);
+    private String filePath = "E:\\gitCodes\\TI1.3-2DGraphics-Opdrachten\\Week4\\001.Verlet\\saves\\SavedVerletEngine.ser";
 
     @Override
     public void start(Stage stage) throws Exception {
         BorderPane mainPane = new BorderPane();
+
+        //make button bar for save and load
+        HBox top = new HBox();
+        top.setSpacing(50);
+        Button saveButton = new Button("Save");
+        Button loadButton = new Button("Load");
+        top.getChildren().addAll(saveButton, loadButton);
+        mainPane.setTop(top);
+
+        //save and load functionality
+        saveButton.setOnAction(this::save);
+        loadButton.setOnAction(this::load);
+
         canvas = new ResizableCanvas(g -> draw(g), mainPane);
         mainPane.setCenter(canvas);
         FXGraphics2D g2d = new FXGraphics2D(canvas.getGraphicsContext2D());
@@ -96,7 +116,7 @@ public class VerletEngine extends Application {
         Particle nearest = getNearest(mousePosition);
         Particle newParticle = new Particle(mousePosition);
         particles.add(newParticle);
-        constraints.add(new DistanceConstraint(newParticle, nearest));
+
 
         if (e.getButton() == MouseButton.SECONDARY) {
             ArrayList<Particle> sorted = new ArrayList<>();
@@ -110,13 +130,31 @@ public class VerletEngine extends Application {
                 }
             });
 
-            constraints.add(new DistanceConstraint(newParticle, sorted.get(2)));
+            if (e.isControlDown()){
+                constraints.add(new DistanceConstraint(newParticle, sorted.get(2), 100));
+//                constraints.add(new RopeConstraint(newParticle, sorted.get(2)));
+//                constraints.add(new RopeConstraint(newParticle, nearest));
+                return;
+            } else if(e.isShiftDown()){
+                constraints.add(new DistanceConstraint(sorted.get(1), sorted.get(2)));
+                particles.remove(newParticle);
+                return;
+            } else {
+                constraints.add(new DistanceConstraint(newParticle, sorted.get(2)));
+            }
+            constraints.add(new DistanceConstraint(newParticle, nearest));
+
         } else if (e.getButton() == MouseButton.MIDDLE) {
             // Reset
             particles.clear();
             constraints.clear();
             init();
+        } else if (e.isControlDown() && e.getButton() == MouseButton.PRIMARY){
+            constraints.add(new PositionConstraint(newParticle));
+        } else {
+            constraints.add(new DistanceConstraint(newParticle, nearest));
         }
+
     }
 
     private Particle getNearest(Point2D point) {
@@ -145,6 +183,34 @@ public class VerletEngine extends Application {
         mouseConstraint.setFixedPosition(new Point2D.Double(e.getX(), e.getY()));
     }
 
+    private void save(ActionEvent event) {
+        try{
+            FileOutputStream fileOutputStream = new FileOutputStream(filePath);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+//            objectOutputStream.writeObject();
+            System.out.println("Saved!");
+            objectOutputStream.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void load(ActionEvent event){
+        try{
+            FileInputStream fis = new FileInputStream(filePath);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void errorNoFileFound(){
+        new Alert(Alert.AlertType.ERROR, "Couldn't find file" +
+                "\nOr no Object found for loading");
+    }
     public static void main(String[] args) {
         launch(VerletEngine.class);
     }
