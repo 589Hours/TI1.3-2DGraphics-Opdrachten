6ed271cb5f4792fea546588b6b63da75c04643bc
589Hours <rik.vanham@ziggo.nl>
2024-03-18 11:20:48 +0100

    Added comments to cases

--- Eindopdracht/src/KeyLogger.java
+++ Eindopdracht/src/KeyLogger.java
@@ -9,22 +9,27 @@ public class KeyLogger {
     public void keyPressed(KeyEvent event){
         String charFromEvent = event.getCharacter();
         switch (charFromEvent){
+            //toggle debug
             case "g":
-                System.out.println("Debug activated");
                 eindopdracht.debugToggle();
                 break;
+            //pause the game
             case " ":
                 eindopdracht.pause();
                 break;
+            //show new stage where you can spawn items
             case "\t":
                 eindopdracht.showSpawnScreen();
                 break;
+            //shoot the weapon you are holding
             case "f":
                 eindopdracht.shoot();
                 break;
+            //explode the explosive you are holding
             case "h":
                 eindopdracht.explode();
                 break;
+            //this is more for debugging since I forgot to turn the keyLogger option on for canvas
             default:
                 System.out.println("Unknown command");
                 break;
